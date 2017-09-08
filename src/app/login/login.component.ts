4e31679caf3119f20d66db689bf0620a8737db9c
tangyy <tangyueyang@lztogether.com>
2017-09-08 13:51:42 +0800

    Mod css to sass

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  username: String = '';
+  password: String = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * 登录
+   */
+  login(formValue) {
+    console.log(formValue);
+  }
 }
