920d17e2494cd6e667097d95aa13fca7e10b2b23
tangyy <tangyueyang@lztogether.com>
2017-09-11 20:41:38 +0800

    Mod root route redirect todo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { TodoComponent } from './todo/todo.component';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'todos',
     pathMatch: 'full'
   },
   {
@@ -14,7 +14,7 @@ export const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: 'todo',
+    path: 'todos',
     component: TodoComponent
   }
 
