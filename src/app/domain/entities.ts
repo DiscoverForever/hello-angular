d538b7b63d2c4b798daf6d3952877cf1f9880161
tangyy <tangyueyang@lztogether.com>
2017-10-01 17:26:26 +0800

    Add file entities

---
+++ src/app/domain/entities.ts
@@ -0,0 +1,18 @@
+export class Todo {
+  id: String;
+  desc: String;
+  state: TodoState;
+  userId: number;
+}
+
+export class User {
+  id: number;
+  username: string;
+  password: string;
+}
+export enum TodoState {
+  BACKLOG,
+  INPROGREESS,
+  DONE,
+  CLOSE
+}
